bc7f5a26d647859ce373973c07da2038c83d692a
Katia Elizabeth Garvich San Martin <kgarvich@isil.isil.edu.pe>
2021-01-23 19:42:47 -0500

    Trabajo final

--- app/Payment/Controllers/PaymentController.php
+++ app/Payment/Controllers/PaymentController.php
@@ -6,14 +6,23 @@ use PayPal\Api\Payment;
 use PayPal\Api\RedirectUrls;
 use PayPal\Api\Transaction;
 use App\Payment\Services\PaymentPaypal;
+use App\Sales\Models\Pedido;
+use DB;
 
 use App\Handlers\Interfaces\PedidoEntityInterface;
 
 class PaymentController
 {
+	//public $invoiceNumber=0;
 	public function paypal(
 		PedidoEntityInterface $pedidoEntityInterface
 	) {
+
+		$userid=1;
+		if(\Cart::session($userid)->getTotalQuantity()==0){
+            return redirect('/cart');
+        }
+		
 
 		$resultPedido = $pedidoEntityInterface->create(request()->all());
 		//print_r($resultPedido); dd();
@@ -46,13 +55,20 @@ class PaymentController
 		$payment->setIntent('sale')
 		    ->setPayer($payer)
 		    ->setTransactions([$transaction])
-		    ->setRedirectUrls($redirectUrls);
-
+			->setRedirectUrls($redirectUrls);
+			
 		try {
 		    $payment->create($objPaypal->getApi());
 		} catch (Exception $e) {
 		    throw new Exception('Unable to create link for payment');
 		}
+
+		$pedido=Pedido::findorfail($resultPedido['id']);
+		$pedido->codigo=$invoiceNumber;
+		$pedido->save();
+		//$userid=1;
+		\Cart::session($userid)->clear();
+		//$this->invoiceNumber=$invoiceNumber;
 
 		header('location:' . $payment->getApprovalLink());
 		exit(1);
@@ -70,7 +86,12 @@ class PaymentController
 
 	public function successBuy()
 	{
+		$id = Pedido::select(DB::RAW("MAX(id) as id"))->first();
+		$codigo = Pedido::select('codigo')->where('id', '=', $id['id'])->first();
 		\Cart::clear();
-		return view("cart.cart_success");
+		
+		return view("cart.cart_success", [
+			'invoiceNumber' => $codigo['codigo']
+			]);
 	}
 }
